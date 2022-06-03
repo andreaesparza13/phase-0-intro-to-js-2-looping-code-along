cacd0294b4401ca7fb00244bd9508f8edc2f14fb
Andrea <acesparza05@gmail.com>
2022-06-03 10:53:06 -0500

    commit tree 996e574888c0196d681f222f38a299fae6434038

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  };
+*/
+
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+const occasion = [];
+const names = [];
+function writeCards(names, occasion) {
+    var messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages = [...messages, `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`];
+    };
+    return messages;
+};
+
+let number;
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    };
+};
